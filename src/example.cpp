27a1c0244331648634311887547ef67bed7cb0f3
Brandon Ros <brandonros1@gmail.com>
2022-09-03 10:20:43 -0400

    logs

--- src/example.cpp
+++ src/example.cpp
@@ -15,18 +15,23 @@ int main(int argc, char* argv[]) {
     ssl_socket socket(io_context, ssl_context);
     tcp::resolver resolver(io_context);
     // resolve
+    std::cout << "Resolve..." << "\n";
     auto endpoints = resolver.resolve("google.com", "443");
     // connect
+    std::cout << "Connect..." << "\n";
     io::connect(socket.next_layer(), endpoints);
     // handshake
+    std::cout << "Handshake..." << "\n";
     socket.handshake(ssl::stream_base::client);
     // write request
+    std::cout << "Write request..." << "\n";
     char request[] =
         "GET / HTTP/1.1\n"
         "Host: www.google.com\n"
         "Connection: close\n\n";
     io::write(socket, io::buffer(request));
     // read response
+    std::cout << "Read response..." << "\n";
     io::streambuf response;
     error_code ec;
     io::read(socket, response, ec);
